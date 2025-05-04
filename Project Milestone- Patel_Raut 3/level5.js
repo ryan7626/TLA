68266fd7ca55e5c4922c357acef47f563aef7588
Kavan9090 <94819957+Kavan9090@users.noreply.github.com>
2025-05-04 19:32:02 -0400

    Add files via upload

---
+++ Project Milestone- Patel_Raut 3/level5.js
@@ -0,0 +1,62 @@
+function startLevel5() {
+    console.log("Starting Level 4...");
+    currentLevel = 5;
+    // Update Level Indicator
+    const levelIndicator = document.getElementById("levelindicator");
+    if (levelIndicator) {
+      levelIndicator.textContent = "Level 5"; // Update to Level 4
+    }
+  
+    // Clear old level data
+    solidRegions.length = 0;
+    spikes.length = 0;
+    tempPlatforms.length = 0;
+    initPlatforms.length = 0;
+  
+    solidRegions.push(
+      { x: -1.0, y: -0.8, width: 2.0, height: 0.1 }, // ground
+    );
+    tempPlatforms.push(
+    { x: -0.2, y: -0.4, width: 0.4, height: 0.05 }, // center platform
+    { x: -0.7, y: -0.2, width: 0.3, height: 0.05 }, // left floating
+    { x: 0.5, y: -0.1, width: 0.3, height: 0.05 } // right floating
+    );
+    initPlatforms.push(
+    { x: -0.2, y: -0.4, width: 0.4, height: 0.05 }, // center platform
+    { x: -0.7, y: -0.2, width: 0.3, height: 0.05 }, // left floating
+    { x: 0.5, y: -0.1, width: 0.3, height: 0.05 } // right floating
+    );
+    
+  
+    spikes.push(
+      { x: -0.4, y: -0.7, dir: "up" },
+      { x: 0.3, y: -0.7, dir: "up" },
+      { x: 0.75, y: -0.05, dir: "up" },
+      { x: 0.15, y: -0.35, dir: "up" }
+    );
+  
+  
+    goal.x = 0.94;
+    goal.y = 0.2;
+    goal.width = 0.03;
+    goal.height = 0.35;
+  
+    button.x = -0.625;
+    button.y = -0.15;
+    button.width = 0.15;
+    button.height = 0.05;
+  
+    // Reset player
+    player.x = -0.8;
+    player.y = -0.7;
+    player.vx = 0;
+    player.vy = 0;
+  
+    // Reset gameplay state
+    goalActivated = false;
+    levelPassed = false;
+    document.getElementById("levelPassedMsg").style.display = "none";
+    currentHint = "Not everything that supports you is here to stay—move quickly, but plan ahead.";
+    document.getElementById("hintBox").textContent = "Press H to reveal Hint";
+  }
+  
